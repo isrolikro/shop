3a844475821530db06c8a51ad3669634cc6d372d
isrolik <isralerosn@gmail.com>
2023-08-06 10:49:15 +0300

    button created that prints button

--- project_shop.js
+++ project_shop.js
@@ -1,2 +1,7 @@
 const body = document.getElementsByTagName('BODY')[0]
 
+const button = document.createElement("button")
+
+button.addEventListener("click", () => {
+    console.log("button clicked")
+})
